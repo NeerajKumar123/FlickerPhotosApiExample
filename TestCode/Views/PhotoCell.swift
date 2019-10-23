2f8e3fc1b815e3f9ed7f953d8c662c01c7be7e66
NeerajKumar123 <neerajkiet@gmail.com>
2019-10-23 16:01:33 +0530

    image reflection done

--- TestCode/Views/PhotoCell.swift
+++ TestCode/Views/PhotoCell.swift
@@ -12,24 +12,7 @@ class PhotoCell: UITableViewCell {
 
     @IBOutlet weak var serachedImage: UIImageView!
     @IBOutlet weak var imageCompleteNameLabel: UILabel!
-    
-    var imageUrl: String = ""{
-        didSet{
-            let queue = DispatchQueue.global(qos: .default)
-            queue.async {
-                let imageDownloader = ImageDownloader()
-                guard let url = URL(string: self.imageUrl) else { return  }
-                imageDownloader.downloadImage(from: url) {(img, err) in
-                    if let image = img {
-                        DispatchQueue.main.sync {
-                            self.serachedImage.image = image
-                        }
-                    }
-                }
-            }
-        }
-    }
-    
+   
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
